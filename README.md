eb818ddf10a47cf24e78a2fa0f591fbf3fc62e13
cursoskanechan <kanes.chan@gmail.com>
2022-06-24 09:24:29 -0300

    feat: Funções para apresentar informações.

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 ---
 
 - Estrutura básica
+- Alterando forms através de: document.getElementById("titulo").innerHTML
+- Funções para apresentar informações:
+   - document.write();
+   - window.alert();
+   - console.log();
--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert("Olá Mundo!");
+document.getElementById("titulo").innerHTML = "Sou um título dinâmico.";
+
+document.write("Algum texto.");
+window.alert("Mensagem de exemplo.");
+console.log("Mensagem que irá aparecer no console.");
